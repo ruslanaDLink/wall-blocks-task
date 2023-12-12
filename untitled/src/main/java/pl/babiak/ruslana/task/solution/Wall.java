ae9672e26f75f4ab7eb50bdf88920610ebb74e7a
Ruslana Babiak <ruslana.babiak@gmail.com>
2023-12-12 12:21:29 +0100

    Added implementation to methods in Wall class.

--- untitled/src/main/java/pl/babiak/ruslana/task/solution/Wall.java
+++ untitled/src/main/java/pl/babiak/ruslana/task/solution/Wall.java
@@ -2,21 +2,52 @@ package pl.babiak.ruslana.task.solution;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
-public class Wall implements Structure{
+public class Wall implements Structure, CompositeBlock {
+    private List<Block> blockList;
 
     @Override
     public Optional<Block> findBlockByColor(String color) {
+        for (Block block : blockList) {
+            if (block.getColor().equals(color)) {
+                return Optional.of(block);
+            }
+        }
         return Optional.empty();
     }
 
     @Override
     public List<Block> findBlocksByMaterial(String material) {
-        return null;
+        return blockList
+                .stream()
+                .filter(block -> block.getMaterial().equals(material))
+                .collect(Collectors.toList());
     }
 
     @Override
     public int count() {
-        return 0;
+        return blockList.size();
+    }
+
+    @Override
+    public String getColor() {
+        for (Block block : blockList) {
+            return block.getColor();
+        }
+        return null;
+    }
+
+    @Override
+    public String getMaterial() {
+        for (Block block : blockList) {
+            return block.getMaterial();
+        }
+        return null;
+    }
+
+    @Override
+    public List<Block> getBlocks() {
+        return blockList;
     }
 }
